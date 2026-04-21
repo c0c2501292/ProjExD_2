b4ce86a76f63ea4938c06c4918cd089b173029bc
c0c2501292 <c0c2501292@edu.teu.ac,jp>
2026-04-21 14:33:39 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,8 @@
 import os
 import sys
+import random  
 import pygame as pg
+
 
 
 WIDTH, HEIGHT = 1100, 650
@@ -22,6 +24,16 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    bb_img = pg.Surface((20, 20))  # 爆弾用の空のSurfaceを作る
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 爆弾円を描く
+    bb_img.set_colorkey((0, 0, 0))  # 爆弾の黒い部分を透過させる
+    bb_rct = bb_img.get_rect()  # 爆弾Rectを取得する
+    bb_rct.centerx = random.randint(0, WIDTH)  # 爆弾の初期横座標を設定する
+    bb_rct.centery = random.randint(0, HEIGHT)  # 爆弾の初期縦座標を設定する
+    vx, vy = +5, +5  # 爆弾の速度
+
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -40,6 +52,8 @@ def main():
 
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)  # 爆弾を移動させる
+        screen.blit(bb_img, bb_rct)  # 爆弾を表示させる
         pg.display.update()
         tmr += 1
         clock.tick(50)
