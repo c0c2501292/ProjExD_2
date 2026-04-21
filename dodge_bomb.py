cfc6b11c6add9324f5b1de3f749643c72577e582
c0c2501292 <c0c2501292@edu.teu.ac,jp>
2026-04-21 16:07:52 +0900

    演習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -66,11 +66,34 @@ def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
     bb_accs = [a for a in range(1, 11)]
     return bb_imgs, bb_accs
 
+def get_kk_imgs() -> dict[tuple[int, int], pg.Surface]:
+    """
+    移動量タプルをキー,rotozoomしたSurfaceを値とした辞書を返す
+    """
+    kk_img = pg.image.load("fig/3.png")
+    # デフォルト（右向き）を基準に反転や回転を行う
+    kk_img_flip = pg.transform.flip(kk_img, True, False) # 右向き
+    
+    kk_dict = {
+        ( 0,  0): pg.transform.rotozoom(kk_img, 0, 0.9),      # 静止（左向き）
+        (-5,  0): pg.transform.rotozoom(kk_img, 0, 0.9),      # 左
+        (-5, -5): pg.transform.rotozoom(kk_img, -45, 0.9),    # 左上
+        ( 0, -5): pg.transform.rotozoom(kk_img_flip, 90, 0.9), # 上（右向きを90度）
+        (+5, -5): pg.transform.rotozoom(kk_img_flip, 45, 0.9), # 右上
+        (+5,  0): pg.transform.rotozoom(kk_img_flip, 0, 0.9),  # 右
+        (+5, +5): pg.transform.rotozoom(kk_img_flip, -45, 0.9),# 右下
+        ( 0, +5): pg.transform.rotozoom(kk_img_flip, -90, 0.9),# 下
+        (-5, +5): pg.transform.rotozoom(kk_img, 45, 0.9),     # 左下
+    }
+    return kk_dict
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("fig/pg_bg.jpg")    
-    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
+    bg_img = pg.image.load("fig/pg_bg.jpg") 
+
+    kk_imgs = get_kk_imgs()
+    kk_img = kk_imgs[(0, 0)]
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
 
@@ -90,7 +113,7 @@ def main():
                 return
         screen.blit(bg_img, [0, 0]) 
 
-        idx = min(tmr // 500, 9)  # 500フレームごとに段階を上げる（最大9）
+        idx = min(tmr // 300, 9)  # 300フレームごとに段階を上げる（最大9）
         bb_img = bb_imgs[idx]
         avx = vx * bb_accs[idx]
         avy = vy * bb_accs[idx]
@@ -102,6 +125,8 @@ def main():
             if key_lst[key]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
+
+        kk_img = kk_imgs[tuple(sum_mv)]
 
         kk_rct.move_ip(sum_mv)
 
